5e71ea6b1266a9a5fafb0995eceb12881460d437
Lucas Gabriel Noleto Milagres <lucasnmilagres@outlook.com>
2022-01-18 17:37:13 +0100

    Update Employer.cs

--- Employer.cs
+++ Employer.cs
@@ -1,39 +1,39 @@
 ﻿public class Employee
 {
-    //These are the class properties
-	public string Name { get; set; }
-	public string Age { get; set; }
-	private string Phone { get; set; }
-	public double Salary { get; set; }
-	public string Address_Street { get; set; }
-	public string AddressCity { get; set; }
-    public string AddressZip { get; set; }
-    public string AddrNumber { get; set; }
-	public Company Company { get; set; }
+	 //These are the class properties
+	 public string Name { get; set; }
+	 public string Age { get; set; }
+	 private string Phone { get; set; }
+	 public double Salary { get; set; }
+	 public string Address_Street { get; set; }
+	 public string AddressCity { get; set; }
+	 public string AddressZip { get; set; }
+	 public string AddrNumber { get; set; }
+	 public Company Company { get; set; }
 
-    //This is the class constructor
-	public Employee(string name, string x, string y, int salary, string street, string city, string zip, string number, Company company)
-    {
-        Name = name;
-        Age = x;
-        Phone = y;
-        Salary = salary;
-        Address_Street = street;
-        //AddrNumber = street.Split('-')[1];
-        AddrNumber = number;
-        AddressCity = city;
-        AddressZip = zip;
-        Company = company;
-    }
+	 //This is the class constructor
+	 public Employee(string name, string x, string y, int salary, string street, string city, string zip, string number, Company company)
+	 {
+		 Name = name;
+		 Age = x;
+		 Phone = y;
+		 Salary = salary;
+		 Address_Street = street;
+		 //AddrNumber = street.Split('-')[1];
+		 AddrNumber = number;
+		 AddressCity = city;
+		 AddressZip = zip;
+		 Company = company;
+	 }
+	 
+	 public int moveAddress(string newCity, string newStreet, string newZip, string newNumber)
+	 {
+		 if (String.IsEmpty(newCity) || String.IsEmpty(newStreet) || String.IsEmpty(newZip) || String.IsEmpty(newNumber))
+			 return -1;
 
-    public int moveAddress(string newCity, string newStreet, string newZip, string newNumber)
-    {
-        if (String.IsEmpty(newCity) || String.IsEmpty(newStreet) || String.IsEmpty(newZip) || String.IsEmpty(newNumber))
-            return -1;
-
-        Address_Street = newStreet;
-        AddrNumber = newNumber;
-        AddressZip = newZip;
-        return 0;
-    }
-}
+		 Address_Street = newStreet;
+		 AddrNumber = newNumber;
+		 AddressZip = newZip;
+		 return 0;
+	 }
+ }
